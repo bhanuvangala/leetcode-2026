8e28cfcff0e130a57c260cd1f005ee92df1dbf8d
bhanuvangala <bhanuvangala23568@gmail.com>
2026-01-27 23:46:14 +0530

    solved leetcode 202

--- leetcode-2026/hashing/202-happy-number.java
+++ leetcode-2026/hashing/202-happy-number.java
@@ -1,26 +1,19 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean isHappy(int n) {
-        HashMap<Integer,Boolean>map=new HashMap<>();
-        while(n!=1){ //run logic until it equals 1
-        //square digits in num
-            int t=n;
-            int sum=0;
-            while(t!=0){
-                int rem=t%10;
-                sum+=rem*rem;
-                t=t/10;
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (n != 1) {
+            if (seen.contains(n)) return false;
+            seen.add(n);
+
+            int sum = 0;
+            while (n > 0) {
+                int rem = n % 10;
+                sum += rem * rem;
+                n /= 10;
             }
-            //loop detect
-            if(map.containsKey(sum)){
-                return false;
-            }else{
-                map.put(sum,false);
-            }
-            n=sum;
+            n = sum;
         }
-        
         return true;
     }
 }
