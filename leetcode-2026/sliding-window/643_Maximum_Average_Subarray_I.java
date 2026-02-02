75aa3a6e572080909cf89ed40537efd921d00cec
bhanuvangala <bhanuvangala23568@gmail.com>
2026-02-02 23:06:16 +0530

    Solve LeetCode 643 using fixed-size sliding window

---
+++ leetcode-2026/sliding-window/643_Maximum_Average_Subarray_I.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+
+        // Slide the window
+        for (int i = k; i < nums.length; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum / k;
+    }
+}
