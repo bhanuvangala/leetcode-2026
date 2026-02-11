cb271dd1dd3500cb9005d992b6d3eb626e3c1596
bhanuvangala <bhanuvangala23568@gmail.com>
2026-02-11 23:43:20 +0530

    Solved LeetCode 844: Backspace String Compare

--- leetcode-2026/greedy/PriorityQueueExample.java
+++ leetcode-2026/greedy/PriorityQueueExample.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class PriorityQueue{
+public class PriorityQueueExample{
     public static void main(String[] args) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
@@ -7,14 +7,11 @@ public class PriorityQueue{
         pq.add(5);
         pq.add(20);
         pq.add(15);
+        int[] ropes = null;
         for(int rope:ropes){
             pq.add(rope);
         }
-        int minCost=0;
         while (pq.size()>1) {
-            int r1 = pq.poll();
-            int r2 = pq.poll();
-            minCost += r1 + r2;
         }
     }
 }
